9fc0ca39f02a7f8cba2c66ef1dd11ab335d377b4
dz2486 <dz2486@columbia.edu>
2021-11-15 21:25:41 -0500

    Add test to Portolio

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,6 +1,22 @@
 from portfolio import Portfolio
+import pytest
+
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost() == 0.0
 
 def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
     assert p.cost() == 17648.0
+
+def test_buy_multiple_stocks():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.15)
+    assert p.cost() == 21263.0
+
+def test_missing_buy_arguments():
+    p = Portfolio()
+    with pytest.raises(TypeError):
+        p.buy("IBM")
