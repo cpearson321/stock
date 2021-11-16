eba2ad4546e4dafd908e569c5d5f935e463ddb8a
dz2486 <dz2486@columbia.edu>
2021-11-15 21:01:48 -0500

    Create pytest file

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,9 +1,6 @@
-# test_portfolio.py
 from portfolio import Portfolio
 
-p = Portfolio()
-print(f"Empty portfolio cost: {p.cost()}")
-p.buy("IBM", 100, 176.48)
-print(f"With 100 IBM @ 176.48: {p.cost()}")
-p.buy("HPQ", 100, 36.15)
-print(f"With 100 HPQ @ 36.15: {p.cost()}")
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
